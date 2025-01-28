ea2f7da01218c90203d636533a127fa6caec930f
Ohmyghaddd <lpAlegado@mcm.edu.ph>
2025-01-28 09:19:24 +0800

    Add files via upload

---
+++ public class Calculator.java
@@ -0,0 +1,20 @@
+public class public class Calculator {
+    
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        double result;
+ 
+        result = num1 + num2;
+        System.out.println("Addition: " + result);
+ 
+        result = num1 - num2;
+        System.out.println("Subtraction: " + result);
+ 
+        result = num1 * num2;
+        System.out.println("Multiplication: " + result);
+ 
+        result = (double) num1 / num2;
+        System.out.println("Division: " + result);
+    }
+}
